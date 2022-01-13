f34e9dbb6cdad044ad98364a92179b315707c031
tharun0120 <81463413+tharun0120@users.noreply.github.com>
2022-01-13 22:01:23 +0530

    minor fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,50 +2,50 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./helpers/PrivateRoute";
-import Loadable from "react-loadable";
-import Loader from "./components/Loader";
-// import LoginPage from "./pages/LoginPage";
-// import RegisterPage from "./pages/RegisterPage";
-// import AccountPage from "./pages/AccountPage";
-// import HomePage from "./pages/HomePage";
-// import Page404 from "./pages/404Page";
-// import AboutPage from "./pages/AboutPage";
+// import Loadable from "react-loadable";
+// import Loader from "./components/Loader";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import AccountPage from "./pages/AccountPage";
+import HomePage from "./pages/HomePage";
+import Page404 from "./pages/404Page";
+import AboutPage from "./pages/AboutPage";
 
-const HomePage = Loadable({
-  loader: () => import("./pages/HomePage"),
-  loading: Loader,
-  delay: 300,
-});
+// const HomePage = Loadable({
+//   loader: () => import("./pages/HomePage"),
+//   loading: Loader,
+//   delay: 300,
+// });
 
-const LoginPage = Loadable({
-  loader: () => import("./pages/LoginPage"),
-  loading: Loader,
-  delay: 300,
-});
+// const LoginPage = Loadable({
+//   loader: () => import("./pages/LoginPage"),
+//   loading: Loader,
+//   delay: 300,
+// });
 
-const RegisterPage = Loadable({
-  loader: () => import("./pages/RegisterPage"),
-  loading: Loader,
-  delay: 300,
-});
+// const RegisterPage = Loadable({
+//   loader: () => import("./pages/RegisterPage"),
+//   loading: Loader,
+//   delay: 300,
+// });
 
-const AccountPage = Loadable({
-  loader: () => import("./pages/AccountPage"),
-  loading: Loader,
-  delay: 300,
-});
+// const AccountPage = Loadable({
+//   loader: () => import("./pages/AccountPage"),
+//   loading: Loader,
+//   delay: 300,
+// });
 
-const Page404 = Loadable({
-  loader: () => import("./pages/404Page"),
-  loading: Loader,
-  delay: 300,
-});
+// const Page404 = Loadable({
+//   loader: () => import("./pages/404Page"),
+//   loading: Loader,
+//   delay: 300,
+// });
 
-const AboutPage = Loadable({
-  loader: () => import("./pages/AboutPage"),
-  loading: Loader,
-  delay: 300,
-});
+// const AboutPage = Loadable({
+//   loader: () => import("./pages/AboutPage"),
+//   loading: Loader,
+//   delay: 300,
+// });
 
 function App() {
   return (
